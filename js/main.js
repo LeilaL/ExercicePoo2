ade31e3a206513656bb0594334bfca042c22adf1
Leila.L <leila.lababsa@hotmail.fr>
2017-08-21 10:59:49 +0200

    create 2 warriors with constructor

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
-var constructeur = {
-  name: "Xena",
-  attack: 7,
-  defense: 8,
-  health: 9,
+// var constructeur = {
+//   name: "Xena",
+//   attack: 7,
+//   defense: 8,
+//   health: 10,
+//   combat : function() {
+//
+//   }
+// }
+
+
+function person(name, attack, defense, health) {
+    this.name = name;
+    this.attack = attack;
+    this.defense = defense;
+    this.health = health;
+    this.combat : function(attaquer) {
+      this.health=attaquer-this.attack;
+    }
+
 }
+var firstWarrior = new person("Xena", 7, 8, 10);
+var secondWarrior = new person("Alti", 6, 7, 10);
+console.log("Vie Xena" + secondWarrior.combat);
+console.log("Vie Alti" + firstWarrior.combat);
