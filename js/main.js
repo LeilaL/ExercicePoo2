d367bc2b383eb5d6da7fc71a3eeb56155bea3cb4
Leila.L <leila.lababsa@hotmail.fr>
2017-08-21 11:27:44 +0200

    create method for reduce health when warrior attack

--- js/main.js
+++ js/main.js
@@ -14,12 +14,12 @@ function person(name, attack, defense, health) {
     this.attack = attack;
     this.defense = defense;
     this.health = health;
-    this.combat : function(attaquer) {
+    this.combat = function(attaquer) {
       this.health=attaquer-this.attack;
-    }
+    };
 
 }
 var firstWarrior = new person("Xena", 7, 8, 10);
 var secondWarrior = new person("Alti", 6, 7, 10);
-console.log("Vie Xena" + secondWarrior.combat);
-console.log("Vie Alti" + firstWarrior.combat);
+console.log("Vie Xena" + ":" + this.combat);
+console.log("Vie Alti" + ":" + this.combat);
